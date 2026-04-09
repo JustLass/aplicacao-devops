ccadf25620b290138978cad687f9114f337af312
erio7 <ericamorim3405@gmail.com>
2026-04-09 20:43:16 -0300

    feat: port listen

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+const PORT = 3000
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'src', 'views', 'index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`)
 })
