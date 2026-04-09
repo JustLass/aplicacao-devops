35a0b806fb31e8da0952a09e8d4dcde4e2f11db3
erio7 <ericamorim3405@gmail.com>
2026-04-09 20:46:43 -0300

    fix: path join src remove

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = 3000
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'views', 'index.html'))
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 app.listen(PORT, () => {
